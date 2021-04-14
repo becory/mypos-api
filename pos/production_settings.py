2da293758c85e29c3d4dcb489cae33e1196913df
yuan <ssuyuan.huang@gmail.com>
2021-04-15 00:24:07 +0800

    s3

--- pos/production_settings.py
+++ pos/production_settings.py
@@ -195,5 +195,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
