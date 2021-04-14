873366505f9eac955b587b6a8b04931db1884630
yuan <ssuyuan.huang@gmail.com>
2021-04-15 02:08:54 +0800

    debug false

--- pos/production_settings.py
+++ pos/production_settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mypos-demo.herokuapp.com']
 
