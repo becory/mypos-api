889631690479d5a33218dfb5751fd31db7428f21
yuan <ssuyuan.huang@gmail.com>
2021-04-14 23:51:11 +0800

    s3

--- pos/production_settings.py
+++ pos/production_settings.py
@@ -192,9 +192,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'api/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'api/static'),
+# ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
